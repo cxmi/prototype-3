9dff714a76907a637df1713e55a3b9ff479a5fec
cxmi <christinexmi@gmail.com>
2025-09-20 21:43:55 -0400

    inputs

--- prototypes-3-up/Assets/Scripts/ScoreKeeping.cs
+++ prototypes-3-up/Assets/Scripts/ScoreKeeping.cs
@@ -1,21 +1,39 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class ScoreKeeping : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    
+    public InputAction anyButtonAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    void OnEnable()
+    {
+        anyButtonAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        anyButtonAction.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
         scoreText.text = ContactLogic.score.ToString();
+        if (anyButtonAction.triggered)
+        {
+            Restart();
+        }
     }
+    
 
     public void Restart()
     {
