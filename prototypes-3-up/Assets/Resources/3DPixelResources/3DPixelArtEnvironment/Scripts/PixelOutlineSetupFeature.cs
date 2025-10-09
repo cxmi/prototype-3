951e29ea234491137512410af6ec1ade52f8a4bf
cxmi <christinexmi@gmail.com>
2025-10-09 17:18:05 -0400

    3d pixel camera

---
+++ prototypes-3-up/Assets/Resources/3DPixelResources/3DPixelArtEnvironment/Scripts/PixelOutlineSetupFeature.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Environment
+{
+    // This one is for Unity 2021, the one for 6 is elsewhere
+    using UnityEngine.Rendering;
+    using UnityEngine.Rendering.Universal;
+
+    /// <summary>
+    /// This render feature creates depth normals and colors after rendering opaques.
+    /// </summary>
+    public class PixelOutlineSetupFeature : ScriptableRendererFeature
+    {
+        public class EmptyPass : ScriptableRenderPass
+        {
+            [Obsolete("This rendering path is for compatibility mode only (when Render Graph is disabled). Use Render Graph API instead.", false)]
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) { }
+        }
+
+        EmptyPass emptyPass;
+
+        public override void Create()
+        {
+            this.emptyPass = new EmptyPass
+            {
+                renderPassEvent = RenderPassEvent.AfterRenderingOpaques
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            this.emptyPass.ConfigureInput(ScriptableRenderPassInput.Depth | ScriptableRenderPassInput.Normal | ScriptableRenderPassInput.Color);
+            renderer.EnqueuePass(this.emptyPass);
+        }
+    }
+}
